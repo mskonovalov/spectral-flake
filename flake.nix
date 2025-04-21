041d2a4f73c0e2efaf3476c4653a435532a7fbcf
jayrovacsek <jay@rovacsek.com>
2025-04-21 15:37:43 +1000

    fix: use approach described in now-cli in handling vercel/pkg binaries (ref: https://github.com/NixOS/nixpkgs/pull/48193/files#diff-329ce6280c48eac47275b02077a2fc62R25)

--- flake.nix
+++ flake.nix
@@ -70,15 +70,46 @@
               chmod +x $out/bin/spectral
             '';
 
-            buildInputs = with pkgs; [
-              stdenv.cc.cc.lib
-            ];
-
-            nativeBuildInputs = with pkgs; [
-              autoPatchelfHook
-            ];
-
             dontStrip = true;
+
+            preFixup =
+              let
+                libPath = pkgs.lib.makeLibraryPath [ pkgs.stdenv.cc.cc ];
+              in
+              ''
+                  orig_size=$(stat --printf=%s $out/bin/spectral)
+                  patchelf --set-interpreter "$(cat $NIX_CC/nix-support/dynamic-linker)" $out/bin/spectral
+                  patchelf --set-rpath ${libPath} $out/bin/spectral
+                  chmod +x $out/bin/spectral
+                  new_size=$(stat --printf=%s $out/bin/spectral)
+                  ###### zeit-pkg fixing starts here.
+                  # we're replacing plaintext js code that looks like
+                  # PAYLOAD_POSITION = '1234                  ' | 0
+                  # [...]
+                  # PRELUDE_POSITION = '1234                  ' | 0
+                  # ^-----20-chars-----^^------22-chars------^
+                  # ^-- grep points here
+
+                #
+                  # var_* are as described above
+                  # shift_by seems to be safe so long as all patchelf adjustments occur 
+                  # before any locations pointed to by hardcoded offsets
+                  var_skip=20
+                  var_select=22
+                  shift_by=$(expr $new_size - $orig_size)
+                  function fix_offset {
+                    # $1 = name of variable to adjust
+                    location=$(grep -obUam1 "$1" $out/bin/spectral | cut -d: -f1)
+                    location=$(expr $location + $var_skip)
+                    value=$(dd if=$out/bin/spectral iflag=count_bytes,skip_bytes skip=$location \
+                               bs=1 count=$var_select status=none)
+                    value=$(expr $shift_by + $value)
+                    echo -n $value | dd of=$out/bin/spectral bs=1 seek=$location conv=notrunc
+                  }
+                  fix_offset PAYLOAD_POSITION
+                  fix_offset PRELUDE_POSITION
+              '';
+
           };
 
           default = self.packages.${system}.spectral-cli;
