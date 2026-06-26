2a77a7728af22aeac2f0b6d8d1af337536f54feb
Jay Rovacsek <jay@rovacsek.com>
2026-06-26 21:03:23 +1000

    Fix autoelf patching (#1)
    * feat: basic amount of hackiness to ensure required libraries exist for the executable, however still hitting issues with the upstream use of pkg
    
    * fix: use approach described in now-cli in handling vercel/pkg binaries (ref: https://github.com/NixOS/nixpkgs/pull/48193/files#diff-329ce6280c48eac47275b02077a2fc62R25)
    
    * feat: add gitignore for result
    
    * fix: resolve build issues on darwin

--- flake.nix
+++ flake.nix
@@ -6,22 +6,35 @@
     flake-utils.url = "github:numtide/flake-utils";
   };
 
-  outputs = { self, nixpkgs, flake-utils }:
-    flake-utils.lib.eachDefaultSystem (system:
+  outputs =
+    {
+      self,
+      nixpkgs,
+      flake-utils,
+    }:
+    flake-utils.lib.eachDefaultSystem (
+      system:
       let
         pkgs = nixpkgs.legacyPackages.${system};
-        
-        # Determine OS and architecture based on system
-        os = if pkgs.stdenv.isDarwin then "macos"
-             else if pkgs.stdenv.isLinux then 
-               (if pkgs.stdenv.buildPlatform.isMusl then "alpine" else "linux")
-             else throw "Unsupported system";
-             
-        arch = if pkgs.stdenv.hostPlatform.isAarch64 then "arm64"
-               else if pkgs.stdenv.hostPlatform.isx86_64 then "x64"
-               else throw "Unsupported architecture";
 
-        version = "6.14.3";  # You can update this version as needed
+        # Determine OS and architecture based on system
+        os =
+          if pkgs.stdenv.isDarwin then
+            "macos"
+          else if pkgs.stdenv.isLinux then
+            (if pkgs.stdenv.buildPlatform.isMusl then "alpine" else "linux")
+          else
+            throw "Unsupported system";
+
+        arch =
+          if pkgs.stdenv.hostPlatform.isAarch64 then
+            "arm64"
+          else if pkgs.stdenv.hostPlatform.isx86_64 then
+            "x64"
+          else
+            throw "Unsupported architecture";
+
+        version = "6.14.3"; # You can update this version as needed
 
         # Define a map of hashes for each platform and architecture combination
         # nix hash to-sri --type sha256  $(nix-prefetch-url https://github.com/stoplightio/spectral/releases/download/v6.14.3/spectral-alpine-arm64)
@@ -36,21 +49,67 @@
 
         # Get the hash for the current os and arch
         hash = builtins.getAttr "${os}-${arch}" hashes;
+
+        name = "spectral-cli";
+
+        src = pkgs.fetchurl {
+          url = "https://github.com/stoplightio/spectral/releases/download/v${version}/spectral-${os}-${arch}";
+          inherit hash;
+        };
       in
       {
         packages = {
-          spectral-cli = pkgs.runCommand "spectral-cli-${version}" {
-            src = pkgs.fetchurl {
-              url = "https://github.com/stoplightio/spectral/releases/download/v${version}/spectral-${os}-${arch}";
-              hash = hash;
-            };
-          } ''
-            mkdir -p $out/bin
-            cp $src $out/bin/spectral
-            chmod +x $out/bin/spectral
-          '';
+          spectral-cli = pkgs.stdenv.mkDerivation {
+            inherit name src version;
+
+            dontUnpack = true;
+
+            installPhase = ''
+              mkdir -p $out/bin
+              cp $src $out/bin/spectral
+              chmod +x $out/bin/spectral
+            '';
+
+            dontStrip = true;
+
+            preFixup = pkgs.lib.optionalString (pkgs.stdenv.isLinux) ''
+                orig_size=$(stat --printf=%s $out/bin/spectral)
+                patchelf --set-interpreter "$(cat $NIX_CC/nix-support/dynamic-linker)" $out/bin/spectral
+                patchelf --set-rpath ${pkgs.lib.makeLibraryPath [ pkgs.stdenv.cc.cc ]} $out/bin/spectral
+                chmod +x $out/bin/spectral
+                new_size=$(stat --printf=%s $out/bin/spectral)
+                ###### zeit-pkg fixing starts here.
+                # we're replacing plaintext js code that looks like
+                # PAYLOAD_POSITION = '1234                  ' | 0
+                # [...]
+                # PRELUDE_POSITION = '1234                  ' | 0
+                # ^-----20-chars-----^^------22-chars------^
+                # ^-- grep points here
+
+              #
+                # var_* are as described above
+                # shift_by seems to be safe so long as all patchelf adjustments occur 
+                # before any locations pointed to by hardcoded offsets
+                var_skip=20
+                var_select=22
+                shift_by=$(expr $new_size - $orig_size)
+                function fix_offset {
+                  # $1 = name of variable to adjust
+                  location=$(grep -obUam1 "$1" $out/bin/spectral | cut -d: -f1)
+                  location=$(expr $location + $var_skip)
+                  value=$(dd if=$out/bin/spectral iflag=count_bytes,skip_bytes skip=$location \
+                             bs=1 count=$var_select status=none)
+                  value=$(expr $shift_by + $value)
+                  echo -n $value | dd of=$out/bin/spectral bs=1 seek=$location conv=notrunc
+                }
+                fix_offset PAYLOAD_POSITION
+                fix_offset PRELUDE_POSITION
+            '';
+
+          };
 
           default = self.packages.${system}.spectral-cli;
         };
-      });
+      }
+    );
 }
